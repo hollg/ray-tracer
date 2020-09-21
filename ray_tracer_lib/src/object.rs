d0a2a44e3ceba0f011b919a184233ec96dab9b89
Gary Holland <garyhollandxyz@gmail.com>
2020-09-21 19:18:37 +0100

    WIP: add object mod

---
+++ ray_tracer_lib/src/object.rs
@@ -0,0 +1,11 @@
+use crate::intersection::Intersection;
+use crate::material::Material;
+use crate::matrix::Matrix;
+use crate::ray::Ray;
+use crate::tuple::Tuple;
+pub trait Object {
+    fn intersect(&self, ray: Ray) -> Result<Vec<Intersection>, ()>;
+    fn normal_at(&self, p: Tuple) -> Tuple;
+    fn transform(&self) -> Matrix;
+    fn material(&self) -> Material;
+}
